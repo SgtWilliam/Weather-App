72377e7dfbe5a37015fa853137c21e3c404db9de
Sgt_William <williampaes097@gmail.com>
2022-02-28 17:22:17 -0300

    Criando Modulos de navegação na API

--- src/modules/LocationID.js
+++ src/modules/LocationID.js
@@ -1,14 +1,14 @@
 const axios = require("axios");
-const API_KEY = "Dt3kfkqDKvwUBoV0ErhMNgoZySAl7LiG"
 
-function LocationID() {
+async function LocationID() {
 
+    const API_KEY = "Dt3kfkqDKvwUBoV0ErhMNgoZySAl7LiG";
     const city = "Brasilia";
 
-    //let res = axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${city}`);
-    let res = axios.get("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=Dt3kfkqDKvwUBoV0ErhMNgoZySAl7LiG&q=Brasilia");
-    let data = res.date;
+    let res = await axios.get(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${city}`);
 
-    console.log(data);
+    let location = res.data
+
+    console.log(location);
 }
 LocationID();
--- src/modules/NavigateAPI.js
+++ src/modules/NavigateAPI.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 const API_KEY = "Dt3kfkqDKvwUBoV0ErhMNgoZySAl7LiG"
 
 async function TemperatureQuery() {
