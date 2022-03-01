b3209935a9b1a17d0a7393bf5411aec22d5255b2
Sgt_William <williampaes097@gmail.com>
2022-03-01 15:24:06 -0300

    Modulo de buscar Graus

--- src/modules/NavigateAPI.js
+++ src/modules/NavigateAPI.js
@@ -1,9 +1,14 @@
-import axios from "axios";
-const API_KEY = "Dt3kfkqDKvwUBoV0ErhMNgoZySAl7LiG"
+const axios = require("axios");
+
+const API_KEY = "4d8fb5b93d4af21d66a2948710284366";
+const id = "Brasilia";
+
 
 async function TemperatureQuery() {
+    let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${id}&appid=${API_KEY}&units=metric`);
 
-    let res = await axios.get()
+    let Info_All = res.data.main.temp ;
 
+    console.log(Info_All);
 };
-
+TemperatureQuery();
