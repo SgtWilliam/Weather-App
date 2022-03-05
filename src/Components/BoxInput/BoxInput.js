58e16abb8d26df06b92123b249145cf3c08e17f9
Sgt_William <williampaes097@gmail.com>
2022-03-05 16:03:20 -0300

    Terminando navegação da API

--- src/Components/BoxInput/BoxInput.js
+++ src/Components/BoxInput/BoxInput.js
@@ -9,44 +9,99 @@ import {
     FormText,
 } from 'reactstrap';
 import './Style.css';
-import '../../Modules/SearchScript'
-import {GetUserCity} from "../../Modules/SearchScript";
-// import {handleClick} from "../BoxInfo/BoxInfo"
 
 
-function BoxInput() {
 
-    const [showConditionOneText, setShowConditionOneText] = useState(false)
+const BoxInput = () => {
 
-    const handleClick = () =>
-        setShowConditionOneText(showConditionOneText => !showConditionOneText)
+    const [city, setCity] = useState("");
+    const [info, setInfo] = useState();
+    const [error, setError] = useState();
+    const [localizedName, setLocalizedName] = useState();
+
+    const apiURL = "http://dataservice.accuweather.com";
+    const apikey = "CC4Zp7rFaDVpDOrtIVTFkAZhfPVT7Nca";
+
+
+
+    const handleInput = (entrada) => {
+        setCity(entrada.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (city === "") return;
+        else {
+            try {
+                const response = await fetch(
+                    `${apiURL}/locations/v1/cities/search?apikey=%20${apikey}&q=${city}`
+                );
+                const data = await response.json();
+
+                const cityKey = data[0].Key;
+                setLocalizedName(data[0].EnglishName);
+                const response2 = await fetch(
+                    `${apiURL}/currentconditions/v1/${cityKey}?apikey=${apikey}`
+                );
+                const data2 = await response2.json();
+                setInfo(data2);
+                setError(null);
+
+
+            } catch (error) {
+                setError(error.message);
+                setInfo(null);
+            }
+        }
+    };
+
 
     return(
         <div>
-            <Form className={"FormCss"}>
-                <FormGroup>
-                    <Label className={"lb-01"} for="nome">Cidade</Label>
-                    <Input
-                        style={{
-                            background: 'white',
-                            color: 'black',
-                        }}
-                        id="input-city"
-                        type="message"
-                        placeholder="Brasilia"
-                        className={"imp-01"}
-                    />
-                    <Button className={"bt-01"} onClick={handleClick}>Submit</Button>
-                </FormGroup>
-            </Form>
-            {showConditionOneText ? ('') : (<div>
+            <div>
+                <Form className={"FormCss"}>
+                    <FormGroup>
+                        <Label className={"lb-01"} for="nome">Cidade</Label>
+                        <Input
+                            style={{
+                                background: 'white',
+                                color: 'black',
+                            }}
+                            onChange={handleInput}
+                            id="input-city"
+                            type="message"
+                            placeholder="Digite o nome de alguma cidade. ex: Brasilia, Fortaleza, Natal"
+                            className={"imp-01"}
+                        />
+                        <Button className={"bt-01"} onClick={handleSubmit}>Submit</Button>
+                    </FormGroup>
+                </Form>
+            </div>
 
-                <h1>O texto deve estar aqui meu nobre :)</h1>
-                <h2>Fala pro bajira para de jogar crypto conflicts</h2>
 
-            </div>)}
+            {error && (
+                <div className="error__container">
+                    <p>City not found!</p>
+                </div>
+            )}
+
+
+            {info && (
+                <div className="Box-Infos">
+                    <h2 className={"cityName"}>{localizedName}</h2>
+                    <div className='box-temp'>
+                        <p className={"emoji"}></p>
+                        <p className={"Temperature"}>{info[0].Temperature.Metric.Value} °C</p>
+                    </div>
+                    <div></div>
+                </div>
+            )}
+
         </div>
     );
-}
+};
+
+
+
 
 export default BoxInput;
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import BoxInput from "../Components/BoxInput/BoxInput";
-import BoxInfo from "../Components/BoxInfo/BoxInfo";
 
-const teste = 22
 
 function HomePage() {
 
